e4240e53bcaa1f485559b1c46d52b78d8a11b8ff
mark <1169593940@qq.com>
2024-08-20 19:40:08 +0800

    增加用户设置功能

---
+++ src/main/java/com/lifecircle/community/controller/UserController.java
@@ -0,0 +1,151 @@
+package com.lifecircle.community.controller;
+
+import com.lifecircle.community.entity.User;
+import com.lifecircle.community.service.UserService;
+import com.lifecircle.community.util.CommunityConstant;
+import com.lifecircle.community.util.CommunityUtil;
+import com.lifecircle.community.util.HostHolder;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Controller
+@RequestMapping("/user")
+public class UserController implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage(){
+        return "/site/setting";
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if(headerImage == null){
+            model.addAttribute("error", "您还没有选择图片.");
+            return "/site/setting";
+        }
+
+        // 检查格式是否正确
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error", "文件的格式不正确");
+            return "/site/setting";
+        }
+
+        // 生成随机文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        // 确定文件存放的路径
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            // 存储文件
+            headerImage.transferTo(dest);
+        }catch (IOException e){
+            logger.error("上传文件失败：" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常!", e);
+        }
+
+        // 更新当前用户的头像路径(web访问路径)
+        // http://localhost:8080/lifecircle/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    @RequestMapping(path = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        // 服务器存放路径
+        fileName = uploadPath + "/" + fileName;
+        // 文件后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        // 响应图片
+        response.setContentType("image/" + suffix);
+        try (
+                FileInputStream fis = new FileInputStream(fileName);
+                OutputStream os = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败: " + e.getMessage());
+        }
+    }
+
+    @RequestMapping(path = "/updatePassword", method = RequestMethod.POST)
+    public String updatePassword(String oldPassword, String newPassword, String confirmPassword, Model model,
+                                 @CookieValue("ticket") String ticket){
+        // 空值处理
+        if(StringUtils.isBlank(oldPassword)){
+            model.addAttribute("oldPasswordMsg", "请输入原密码！");
+            return "site/setting";
+        }
+        if(StringUtils.isBlank(newPassword)){
+            model.addAttribute("newPasswordMsg","请输入新密码！");
+            return "site/setting";
+        }
+        if(StringUtils.isBlank(confirmPassword)) {
+            model.addAttribute("confirmPasswordMsg", "请输入确认密码！");
+            return "site/setting";
+        }
+
+        User user = hostHolder.getUser();
+
+        if(!CommunityUtil.md5(oldPassword + user.getSalt()).equals(user.getPassword())){
+            model.addAttribute("oldPasswordMsg", "原密码不正确！");
+            return "site/setting";
+        }
+
+        if(CommunityUtil.md5(newPassword + user.getSalt()).equals(user.getPassword())){
+            model.addAttribute("newPasswordMsg", "新密码与原密码一致！");
+            return "site/setting";
+        }
+
+        if(!newPassword.equals(confirmPassword)){
+            model.addAttribute("confirmPasswordMsg","两次密码不一致!");
+            return "site/setting";
+        }
+
+        userService.updatePassword(user.getId(), CommunityUtil.md5(newPassword + user.getSalt()));
+        userService.logout(ticket);
+
+        return "redirect:/login";
+    }
+
+}
--- src/main/java/com/lifecircle/community/service/UserService.java
+++ src/main/java/com/lifecircle/community/service/UserService.java
@@ -163,4 +163,12 @@ public class UserService implements CommunityConstant {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
+    public int updateHeader(int userId, String headerUrl){
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+
+    public int updatePassword(int userId, String password){
+        return userMapper.updatePassword(userId, password);
+    }
+
 }
