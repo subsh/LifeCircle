d14938f784cb8e09cf121f64c15e22b4863e3eb1
mark <1169593940@qq.com>
2024-08-20 11:49:46 +0800

    增加生成验证码功能

--- src/main/java/com/lifecircle/community/controller/LoginController.java
+++ src/main/java/com/lifecircle/community/controller/LoginController.java
@@ -1,8 +1,13 @@
 package com.lifecircle.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.lifecircle.community.entity.User;
 import com.lifecircle.community.service.UserService;
 import com.lifecircle.community.util.CommunityConstant;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +15,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage(){
@@ -57,6 +71,25 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response , HttpSession session ){
+        // 生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        // 将验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        // 将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败：" + e.getMessage());
+        }
     }
 
 }
--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,5 @@
+var CONTEXT_PATH = "/lifecircle";
+
 window.alert = function(message) {
 	if(!$(".alert-box").length) {
 		$("body").append(
