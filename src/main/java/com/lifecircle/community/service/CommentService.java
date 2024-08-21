8eb7200ad0f3a6ae7021cf02a19054ab0e8dc5a9
mark <1169593940@qq.com>
2024-08-21 11:53:59 +0800

    增加评论功能

--- src/main/java/com/lifecircle/community/service/CommentService.java
+++ src/main/java/com/lifecircle/community/service/CommentService.java
@@ -2,16 +2,28 @@ package com.lifecircle.community.service;
 
 import com.lifecircle.community.dao.CommentMapper;
 import com.lifecircle.community.entity.Comment;
+import com.lifecircle.community.util.CommunityConstant;
+import com.lifecircle.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit){
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
@@ -19,5 +31,27 @@ public class CommentService {
 
     public int findCommentCount(int entityType, int entityId){
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    // 声明式事务管理，isolation:隔离级别,
+    // propagation:传播机制，这表示如果当前没有事务，就新建一个事务；如果已经存在一个事务中，该方法将会加入到这个事务中。
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment){
+        if(comment == null){
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+
+        // 添加评论
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // 更新帖子评论数量(这里需要判断添加的是评论还是回复)
+        if(comment.getEntityType() == ENTITY_TYPE_POST){
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+
+        return rows;
     }
 }
